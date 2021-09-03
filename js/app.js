2f64c3daa3cd26bd33f06d960dc18783c5ab4d04
kamrul <kamrul1way@gmail.com>
2021-09-03 19:36:05 +0600

    Okay

---
+++ js/app.js
@@ -0,0 +1,23 @@
+const API_KEY = `2a8994973d2eb5bf611d333e0f3164ae`;
+const searchTemperature = () => {
+    const city = document.getElementById('city-name').value;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
+
+   fetch(url)
+    .then(res => res.json())
+    .then(data => displayTemperature(data));
+}
+
+const setInnerText = (id, text) => {
+    document.getElementById(id).innerText = text;
+}
+
+const displayTemperature = temperature => {
+    setInnerText('city', temperature.name);
+    setInnerText('tempracher', temperature.main.temp);
+    setInnerText('condition', temperature.weather[0].main);
+    const url = `https://openweathermap.org/img/wn/${temperature.weather[0].icon}@2x.png`;
+    const imgIcon = document.getElementById('weather-icon');
+    imgIcon.setAttribute('src', url);
+    
+}
